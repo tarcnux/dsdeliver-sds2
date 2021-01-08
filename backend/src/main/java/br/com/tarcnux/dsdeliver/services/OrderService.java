ce5166d6f73e2b7c30227f404e11b0d28736977d
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-08 01:21:20 -0300

    POST /orders

--- backend/src/main/java/br/com/tarcnux/dsdeliver/services/OrderService.java
+++ backend/src/main/java/br/com/tarcnux/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package br.com.tarcnux.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.tarcnux.dsdeliver.dto.OrderDTO;
+import br.com.tarcnux.dsdeliver.dto.ProductDTO;
 import br.com.tarcnux.dsdeliver.entities.Order;
+import br.com.tarcnux.dsdeliver.entities.OrderStatus;
+import br.com.tarcnux.dsdeliver.entities.Product;
 import br.com.tarcnux.dsdeliver.repositories.OrderRepository;
+import br.com.tarcnux.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -18,10 +23,27 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository; 
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		//Product List conversion to ProductDTO with Lambda function & map
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(),
+				dto.getLongitude(),Instant.now(), OrderStatus.PENDING);
+		
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
