b9a23f2fe52de233a86a429c6232e05ed746f1b7
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-09 16:29:25 -0300

    Product select

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -5,11 +5,18 @@ import { useEffect, useState } from 'react';
 import { OrderLocationData, Product } from './types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 
 function Orders() {
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    },0)
 
     useEffect(() => {
         fetchProducts()
@@ -17,12 +24,33 @@ function Orders() {
         .catch(error => console.log(error))
     }, []);
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product);
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return (
+        <>
         <div className="orders-container">
             <StepsHeader />
-            <ProductsList products={products} />
+            <ProductsList 
+                products={products} 
+                onSelectProduct={handleSelectProduct}
+                selectedProducts={selectedProducts}
+                />
             <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
+            <OrderSummary 
+                amount={selectedProducts.length} 
+                totalPrice={totalPrice} />            
         </div>
+        <Footer />
+        </>
     )
 }
 
