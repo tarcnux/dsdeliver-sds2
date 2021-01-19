1c935ec720445b05dd9459eed8ca20a17690b127
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-19 16:59:37 -0300

    Add Home

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
+import Home from './src/Home';
 
 export default function App() {
 
@@ -17,9 +18,10 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Header />
-      <Text>Alo HA Mundo 🌎! </Text>
       <StatusBar style="auto" />
+      <Header />
+      <Home />
+      
     </View>
   );
 }
