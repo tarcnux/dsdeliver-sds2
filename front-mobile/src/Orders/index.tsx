d21abe50c2aafd54ad0a8732d49bed31908a6ee7
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-20 01:28:11 -0300

    Add order list

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import Header from '../Header';
+import OrderCard from '../OrderCard';
 
 
 export default function Orders() {
   return (
     <>
     <Header />
-    <View >
-        <Text>Listagem de Pedidos</Text>
-    </View>
+    <ScrollView style={styles.container}>
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+    </ScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+      paddingLeft: '5%',
+      paddingRight: '5%',
+    }
   });
