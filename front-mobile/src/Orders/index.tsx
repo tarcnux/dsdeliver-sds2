0c0c1d15ee8fb54e0a68c09002c368c32de111f8
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-21 00:20:39 -0300

    List of Orders

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Alert, Text } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
 
 export default function Orders() {
+
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(() => Alert.alert('Erro ao buscar os pedidos.'))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
     <Header />
     <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoading ? (
+          <Text>Carregando pedidos...</Text>
+        ) : (
+          orders.map(order => (
+            <TouchableWithoutFeedback key={order.id}>
+              <OrderCard order={order} />
+            </TouchableWithoutFeedback>
+          ))
+        )}
     </ScrollView>
     </>
   );
